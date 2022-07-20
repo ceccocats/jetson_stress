ba64b6943540af4f744bb6e31e02d6fa198b5009
Francesco <gattifrancesco@hotmail.it>
2022-07-20 18:01:14 +0200

    tegra test ok

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-tegrastats |  while IFS= read -r line; do printf '%s %s\n' "$(date '+%s%N')" "$line"; done
+sudo nvpmodel -m8
+sudo jetson_clocks --fan
+sudo nvpmodel -q
+
+tegrastats |  while IFS= read -r line; do printf '%s %s\n' "$(date '+%s%N')" "$line"; done | tee temps.txt &
+./test
+
+jobs -p | xargs -r kill
